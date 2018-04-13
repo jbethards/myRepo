15964a0012125eac3f47ece0609d387c67cfac11
jbethards <jbethards2984@smail.pcd.edu>
2018-04-12 19:38:08 -0700

    Added code to the helloWorld file to make it valid html

--- helloWorld.php
+++ helloWorld.php
@@ -5,5 +5,18 @@
  * Date: 4/12/2018
  * Time: 7:12 PM
  */
-echo 'Hello, world!';
+
 ?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP Hello</title>
+</head>
+<body>
+<?php
+echo '<p>Hello World</p>'
+?>
+
+</body>
+
+</html>
